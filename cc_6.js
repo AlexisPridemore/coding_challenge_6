ddda2c5b068d421efda90181e7f0aa7267b7f7f8
Alexis Pridemore <alpridemore@usf.edu>
2025-02-08 14:26:33 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,14 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 
 calculateProfit(20, 30, 100);   //Test Data
 calculateProfit(50, 70, 200);   //Test Data
+
+// Task 2 Function Expression, Scenario: Sales Tax Computation
+
+function calculateSalesTax(amount, taxRate) {
+    let salesTax = amount * taxRate;
+    let roundedSalesTax = Math.floor(salesTax);
+    console.log(`Sales Tax: $${roundedSalesTax}`);  
+};  //calculate sales tax
+
+calculateSalesTax(100, 0.07);  //Test Data
+calculateSalesTax(500, 0.1);  //Test Data
