1554dfa28e43e521683f5567e0295264140146db
Alexis Pridemore <alpridemore@usf.edu>
2025-02-08 15:04:41 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -18,3 +18,22 @@ function calculateSalesTax(amount, taxRate) {
 
 calculateSalesTax(100, 0.07);  //Test Data
 calculateSalesTax(500, 0.1);  //Test Data
+
+// Task 3 Arrow Function, Scenario: Employee Bonus Calculation
+
+let calculateBonus = (salary, performanceRating) => {
+    let bonus = 0;
+
+    // Determine bonus based on performance rating
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.20;
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.10;
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05;
+    }
+    console.log(`Bonus: $${bonus}`);
+};
+
+calculateBonus(5000, "Excellent");  //Test Data
+calculateBonus(7000, "Good");       //Test Data
